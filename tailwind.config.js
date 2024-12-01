805ab79ffd1c336b0e90273d8e6a06e4a9ebe0c3
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-01 11:11:00 +0100

    Add Figma design colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#1E90FF', // Example for blue
+        primary: '#F5F5DC', // Example for beige
+        'text-main': '#333333', // Example for main text
+        'text-secondary': '#777777', // Example for secondary text
+      },
+    },
   },
   plugins: [],
 }
