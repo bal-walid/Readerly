214d6bce87988e4218775fe42bb6374c9ebb6fcd
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-04 20:33:14 +0100

    Implement BookCard styling

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,10 +1,11 @@
 const BookCard = ({book}) => {
   return (
-    <div className="border-2 border-black">
-      <p>{book.title}</p>
-      <p>{book.author}</p>
-      <img src={book.cover} alt="" />
-      <p>{book.publishDate}</p>
+    <div className="bg-white w-fit py-2 px-3 rounded-xl">
+      <div className="w-32">
+        <img className="w-full object-cover rounded-lg" src={book.cover} alt="" />
+        <p className="mt-2 whitespace-nowrap overflow-hidden overflow-ellipsis w-full">{book.title}</p>
+        <p className="text-sm text-text-secondary">{book.author}, {book.publishDate}</p>
+      </div>
     </div>
   );
 }
