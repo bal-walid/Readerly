832b30eaf59aa6da9941df92e0fbb6bdb5729f47
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-04 19:30:46 +0100

    Fix body overflow issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import './output.css'
 import Navbar from './components/Navbar'
+import SearchInput from './components/SearchInput'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <main className='flex-1 bg-primary'></main>
+      <main className='flex-1 overflow-y-auto bg-primary'>
+        <SearchInput/>
+        <Outlet/>
+      </main>
     </>
   )
 }
