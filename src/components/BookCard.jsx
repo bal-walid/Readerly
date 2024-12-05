b7d47129de888c147cbb3d42d408d5c086972dab
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-05 20:26:07 +0100

    Add modal displaying logic

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,6 +1,6 @@
-const BookCard = ({book}) => {
+const BookCard = ({book, onClick}) => {
   return (
-    <div className="bg-white w-fit py-2 px-3 rounded-xl">
+    <div onClick={onClick} className="bg-white w-fit py-2 px-3 rounded-xl">
       <div className="w-32">
         <img className="border-[1px] border-silver w-full h-48 object-cover rounded-lg" src={book.cover} alt="" />
         <p className="w-11/12 mt-2 whitespace-nowrap overflow-hidden overflow-ellipsis">{book.title}</p>
