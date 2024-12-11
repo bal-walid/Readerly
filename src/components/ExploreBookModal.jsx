d688269786b83c5303e25c9eeabeef4d788f84fd
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-11 16:23:52 +0100

    Adjust modal body height and width

--- src/components/ExploreBookModal.jsx
+++ src/components/ExploreBookModal.jsx
@@ -5,14 +5,14 @@ import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 const ExploreBookModal = ({ book, close }) => {
   console.log(book);
   return (
-    <div className="absolute z-10 bg-black bg-opacity-50 top-0 left-0 w-full h-full">
-      <div className="bg-white p-6">
+    <div  className="absolute z-10 bg-black bg-opacity-50 top-0 left-0 w-full h-full flex items-center justify-center">
+      <div className="bg-white p-6 w-[80%] h-[80%]">
         {/* Modal Header */}
         <div className="flex items-center">
           <h2 className="text-main text-2xl font-header font-semibold whitespace-nowrap overflow-hidden text-ellipsis">
             {book.title}
           </h2>
-          <div className="flex ml-auto gap-3">
+          <div className="flex ml-auto gap-5">
             <button className="btn text-green-500 flex items-center gap-2 text-sm">
               <BookmarkAddOutlinedIcon fontSize="small" /> Add To Shelf
             </button>
