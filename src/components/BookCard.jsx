2828aed2e1e8be92312d6a610f0e952b9d559fb9
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-11 16:41:10 +0100

    Change api response mapping function to return cover ID instead of cover url

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,8 +1,9 @@
 const BookCard = ({book, onClick}) => {
+  const coverUrl = book.cover ? `https://covers.openlibrary.org/b/id/${book.cover}-M.jpg` : null;
   return (
     <div onClick={onClick} className="bg-white w-fit py-2 px-3 rounded-xl">
       <div className="w-32">
-        <img className="border-[1px] border-silver w-full h-48 object-cover rounded-lg" src={book.cover} alt="" />
+        <img className="border-[1px] border-silver w-full h-48 object-cover rounded-lg" src={coverUrl} alt="" />
         <p className="w-11/12 mt-2 whitespace-nowrap overflow-hidden overflow-ellipsis">{book.title}</p>
         <p className="text-sm text-text-secondary whitespace-nowrap overflow-hidden overflow-ellipsis"><span>{book.author}</span>, {book.publishDate}</p>
       </div>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,26 +1,37 @@
-const API_BASE = `https://openlibrary.org/search.json?`
-const default_fields= "&fields=key,title,author_name,first_publish_year,cover_i"
-const pagination_params = (limit, page) => `&limit=${limit}&page=${page}` 
-
+const API_BASE = `https://openlibrary.org/`;
+const default_fields =
+  "&fields=key,title,author_name,author_key,first_publish_year,cover_i";
+const pagination_params = (limit, page) => `&limit=${limit}&page=${page}`;
 
 const mapBooksResponse = (data) => {
   return data.docs.map((doc) => ({
-    key: doc.key.replace('/works/', ''),
+    key: doc.key.replace("/works/", ""),
     title: doc.title,
-    author: doc.author_name ? doc.author_name[0] : 'Unknown Author',
-    cover: doc.cover_i ? `https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg` : null,
-    publishDate: doc.first_publish_year || 'Unknown Year',
+    author: doc.author_name ? doc.author_name[0] : "Unknown Author",
+    authorId: doc.author_key ? doc.author_key[0] : null,
+    cover: doc.cover_i || null,
+    publishDate: doc.first_publish_year || "Unknown Year",
   }));
 };
 
-export const fetchByCriteria = async (criteria, query, limit = 15, page = 1) => {
-  const url = `${API_BASE}${criteria}=${query}${default_fields}${pagination_params(limit, page)}`; // Fix: replaced `title` with `query`
+export const fetchByCriteria = async (
+  criteria,
+  query,
+  limit = 15,
+  page = 1
+) => {
+  const url = `${API_BASE}search.json?${criteria}=${query}${default_fields}${pagination_params(
+    limit,
+    page
+  )}`;
 
   try {
     const response = await fetch(url);
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+      throw new Error(
+        `Failed to fetch books: ${response.status} ${response.statusText}`
+      );
     }
 
     const data = await response.json();
@@ -30,6 +41,4 @@ export const fetchByCriteria = async (criteria, query, limit = 15, page = 1) => 
     throw error;
   }
 };
-
-
 
