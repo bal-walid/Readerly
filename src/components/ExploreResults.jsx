cca27a43402ae69f7c439dfc01458f228c18e2af
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-11 16:14:37 +0100

    Pass book as prop to ExploreModal

--- src/components/ExploreResults.jsx
+++ src/components/ExploreResults.jsx
@@ -17,14 +17,14 @@ const ExploreResults = () => {
   if (data) {
     return (
       <>
-        {selectedBook && <ExploreBookModal close={() => setSelectedBook(null)} id={selectedBook}/>}
+        {selectedBook && <ExploreBookModal close={() => setSelectedBook(null)} book={selectedBook}/>}
         <h1 className="font-header font-semibold text-3xl capitalize mt-14 mb-5">
           Search Results for: {query}
         </h1>
         <div className="grid grid-cols-[repeat(auto-fit,_minmax(170px,_1fr))] gap-6 pb-4 overflow-y-auto">
           {data.map((book) => (
             <BookCard
-              onClick={() => setSelectedBook(book.key)}
+              onClick={() => setSelectedBook(book)}
               key={book.key}
               book={book}
             />
