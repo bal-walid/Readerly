bedee210c19d897c316ab5fd06de32c02d21cf36
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-11 16:14:15 +0100

    Add btn-shadow utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ export default {
     },
     boxShadow: {
       'nav-shadow': '3px 0px 5px 0px rgba(0, 0, 0, 0.25)',
+      'btn-shadow': '0px 0px 4px 0px rgba(0, 0, 0, 0.25)'
     },
   },
   plugins: [],
