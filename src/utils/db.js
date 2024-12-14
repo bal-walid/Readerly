fd52e9472ebd135584eb7fa0719a787a80acc12b
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-14 19:51:12 +0100

    Add status key to shelf schema

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,16 +1,22 @@
 import Dexie from "dexie";
 const db = new Dexie("books");
 db.version(1).stores({
-  books:
-    "++id, key, title, publishDate, authorId, author, authorBio, synopsis, cover",
+  shelf:
+    "++id, status, key, title, publishDate, authorId, author, authorBio, synopsis, cover",
   wishlist:
     "++id, key, title, publishDate, authorId, author, authorBio, synopsis, cover",
 });
 
 db.open();
 
+db.shelf.hook("creating", (primKey, obj) => {
+  if (!obj.status) {
+    obj.status = "Not Read";
+  }
+});
+
 const addBookToShelf = async (book, authorBio, synopsis) => {
-  return await db.books.add({ ...book, authorBio, synopsis });
+  return await db.shelf.add({ ...book, authorBio, synopsis });
 };
 
 const addBookToWishlist = async (book, authorBio, synopsis) => {
