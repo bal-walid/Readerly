7bb24f71ebdd9183c6c40de7519fd6e03a249db3
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-18 19:17:34 +0100

    Adjust ShelfModal layout

--- src/components/ShelfModal.jsx
+++ src/components/ShelfModal.jsx
@@ -5,6 +5,7 @@ import ModalWrapper from "./ModalWrapper";
 import CloseIcon from "@mui/icons-material/Close";
 import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import StarIcon from "@mui/icons-material/Star";
 
 const ShelfModal = () => {
@@ -28,63 +29,45 @@ const ShelfModal = () => {
   return (
     <ModalWrapper onClose={close}>
       {/* Header */}
-      <div
-        className="bg-white rounded-md p-6 w-[80%] h-[80%] max-w-[1000px] flex flex-col"
-        onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
-      >
-        <div className="flex items-center">
-          <h2 className="text-main text-2xl font-header font-semibold whitespace-nowrap overflow-hidden text-ellipsis">
-            {book.title}
-          </h2>
-          <div className="flex ml-auto gap-5">
-            <button className="text-main">
-              <CloseIcon onClick={close} fontSize="large" />
-            </button>
-          </div>
-        </div>
-        <div className="text-text-secondary">
-          {book.author}, {book.publishDate}
-        </div>
-
-        {/* Main */}
-        <div className="pt-2 flex gap-9 flex-1 min-h-0">
-          <img
-            className="w-80 object-cover h-full border-[2px] border-silver rounded-md"
-            src={coverUrl}
-            alt="Book Cover"
-          />
-          <div className="h-full grid grid-rows-[auto_minmax(0,1fr)_minmax(0,1fr)]">
-            {/* Ratings */}
-            <p className="flex items-center gap-3">
-              <StarIcon
+      <div className="bg-white h-full w-full flex items-center justify-center">
+        <div
+          className="bg-primary rounded-md p-6 w-[90%] h-[90%] max-w-[1200px] flex flex-col"
+          onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
+        >
+          <div className="flex items-center">
+            <h2 className="flex items-center gap-2 text-main text-2xl font-header font-semibold whitespace-nowrap overflow-hidden text-ellipsis">
+              <ArrowBackIcon
+                className="cursor-pointer"
+                onClick={close}
                 fontSize="large"
-                className={book.rating ? "text-main" : "text-text-secondary"}
               />
-              {book.rating ? (
-                <>
-                  <span>
-                    {book.rating}{" "}
-                    <span className="font-semibold text-lg">/ 5</span>
-                  </span>
-                  Average Rating on OpenLibrary
-                  <span className="text-secondary text-sm italic">
-                    ({book.ratingCount} reviews)
-                  </span>
-                </>
-              ) : (
-                <>No Reviews</>
-              )}
-            </p>
-
-            {/* Synopsis */}
-            <div className="flex flex-col">
-              <h3 className="text-2xl mt-3">Synopsis</h3>
-              <p
-                className="book-info break-anywhere overflow-y-auto scrollbar pr-3"
-                dangerouslySetInnerHTML={{
-                  __html: book.synopsis,
-                }}
-              ></p>
+              {book.title}
+            </h2>
+            <div className="flex ml-auto gap-5"></div>
+          </div>
+          {/* Main */}
+          <div className="pt-6 flex gap-9 flex-1 min-h-0">
+            <img
+              className="w-80 object-cover h-full border-[2px] border-silver rounded-md"
+              src={coverUrl}
+              alt="Book Cover"
+            />
+            <div className="h-full grid grid-rows-[minmax(0,1fr)_minmax(0,1fr)]">
+              {/* Synopsis */}
+              <div className="flex flex-col">
+                <h3 className="text-2xl mt-3 font-semibold">Synopsis</h3>
+                <p
+                  className="book-info break-anywhere overflow-y-auto scrollbar pr-3"
+                  dangerouslySetInnerHTML={{
+                    __html: book.synopsis,
+                  }}
+                ></p>
+              </div>
+              {/* Notes */}
+              <div className="flex flex-col">
+                <h3 className="text-2xl mt-3 font-semibold">Your Notes</h3>
+                <div className="overflow-y-auto scrollbar pr-3"></div>
+              </div>
             </div>
           </div>
         </div>
