c9523f3a8858d1a815c3a78df03fbbb0b359131a
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-18 19:27:18 +0100

    Change BookGrid to auto-fill to take up less space in shelf

--- src/components/BookGrid.jsx
+++ src/components/BookGrid.jsx
@@ -2,7 +2,7 @@ import BookCard from "./BookCard";
 
 const BookGrid = ({ books, onCardClick }) => {
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,_minmax(170px,_1fr))] gap-6 pb-4 overflow-y-auto">
+    <div className="grid grid-cols-[repeat(auto-fill,_minmax(170px,_1fr))] gap-6 pb-4 overflow-y-auto">
       {books.map((book) => (
         <BookCard
           onClick={onCardClick ? () => onCardClick(book) : undefined}
