1917e5dba5657280c9ade58c58cd222b0a93f54a
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-19 20:01:09 +0100

    Add dropdown and search input to shelfbooks

--- src/components/Shelf.jsx
+++ src/components/Shelf.jsx
@@ -7,7 +7,7 @@ const Shelf = () => {
     <h1 className="primary-header">Your Shelf</h1>
     <h2 className="secondary-header my-4">Your Statistics</h2>
       <ShelfStats/>
-    <h2 className="secondary-header my-4">Your Books</h2>
+    
       <ShelfBooks/>
     </>
   );
--- src/components/ShelfBooks.jsx
+++ src/components/ShelfBooks.jsx
@@ -3,24 +3,39 @@ import useFetch from "../hooks/useFetch";
 import { getShelfBooks } from "../utils/db";
 import BookGrid from "./BookGrid";
 import router from "../main";
+import BookStatusDropdown from "./BookStatusDropdown";
+import SearchInput from "./SearchInput";
 
 const ShelfBooks = () => {
   const [books, loading, error] = useFetch(getShelfBooks);
   console.log(books);
   const openBookModal = (book) => {
-    router.navigate(`./${book.id}`, {state: {book}})
-  }
+    router.navigate(`./${book.id}`, { state: { book } });
+  };
   const closeBookModal = () => {
-    router.navigate('/shelf');
-  }
-  if (loading) {
-    return "Loading";
-  }
+    router.navigate("/shelf");
+  };
+
   return (
     <>
-      {books && <BookGrid onCardClick={openBookModal} books={books}/>}
-      <Outlet context={{close: closeBookModal}}/>
+      <h2 className="secondary-header my-6 flex items-center gap-8">
+        Your Books{" "}
+        <div className="flex gap-4 w-3/5">
+          <BookStatusDropdown
+            className={
+              "flex items-center text-md p-2  bg-white rounded-full shadow-btn-shadow cursor-pointer"
+            }
+            defaultValue={"Completed"}
+            onStatusChange={() => console.log("a")}
+            filterDropdown={true}
+          />
+          <SearchInput />
+        </div>
+      </h2>
+      {loading && "Loading"}
+      {books && <BookGrid onCardClick={openBookModal} books={books} />}
+      <Outlet context={{ close: closeBookModal }} />
     </>
   );
-}
+};
 export default ShelfBooks;
