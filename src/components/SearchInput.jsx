3bb12f3411eff21f23b02b36671adb9f59d0d530
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-19 19:42:32 +0100

    Adapt SearchInput to shelf filtering

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -3,39 +3,61 @@ import { useNavigate } from "react-router-dom";
 import SearchIcon from '@mui/icons-material/Search';
 import Chevron from '@mui/icons-material/KeyboardArrowDown';
 
-const SearchInput = ({handleSearch}) => {
+const SearchInput = ({ handleSearch, instantSearch = false }) => {
   const [query, setQuery] = useState("");
   const [criteria, setCriteria] = useState("title");
+
   const handleQueryChange = (event) => {
-    setQuery(event.target.value);
+    const newQuery = event.target.value;
+    setQuery(newQuery);
+
+    if (instantSearch) {
+      handleSearch(newQuery, criteria); // Call handleSearch directly if instantSearch is true
+    }
   };
+
   const handleCriteriaChange = (event) => {
     setCriteria(event.target.value);
   };
 
+  const handleKeyUp = (e) => {
+    if (!instantSearch && e.key === "Enter") {
+      handleSearch(query, criteria);
+    }
+  };
+
+  const handleButtonClick = () => {
+    if (!instantSearch) {
+      handleSearch(query, criteria);
+    }
+  };
+
   return (
     <div className="flex text-[#4D4D4D]">
-        <select
-          value={criteria}
-          onChange={handleCriteriaChange}
-          className="text-[#4D4D4D] bg-[#F8F5F5] rounded-l-full text-center outline-none py-2 px-3"
-        >
-          <option value="title">Title</option>
-          <option value="author">Author</option>
-          <option value="subject">Subject</option>
-        </select>
+      <select
+        value={criteria}
+        onChange={handleCriteriaChange}
+        className="text-[#4D4D4D] bg-[#F8F5F5] rounded-l-full text-center outline-none py-2 px-3"
+      >
+        <option value="title">Title</option>
+        <option value="author">Author</option>
+        <option value="subject">Subject</option>
+      </select>
 
       <input
         type="text"
         value={query}
         onChange={handleQueryChange}
-        onKeyUp={(e) => e.key === 'Enter' && handleSearch(query, criteria)}
+        onKeyUp={handleKeyUp}
         placeholder="Search..."
         className="py-2 px-3 outline-none"
       />
 
-      <button onClick={() => handleSearch(query, criteria)} className="text-main flex-items-center py-2 px-3 bg-white rounded-r-full outline-none">
-        <SearchIcon sx={{strokeWidth: 0}} fontSize="small"/>
+      <button
+        onClick={handleButtonClick}
+        className="text-main flex-items-center py-2 px-3 bg-white rounded-r-full outline-none"
+      >
+        <SearchIcon sx={{ strokeWidth: 0 }} fontSize="small" />
       </button>
     </div>
   );
