7b13bdbcaf165835e08f21f38202dfceb912215a
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-19 19:38:57 +0100

    Lift handleSearch function from SearchInput to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import './output.css'
 import Navbar from './components/Navbar'
 import SearchInput from './components/SearchInput'
 import { Outlet } from 'react-router-dom'
+import router from "./main"
 
 function App() {
-
+  const handleSearch = (query, criteria) => {
+    router.navigate(`/explore?criteria=${criteria}&query=${query}`);
+  }
   return (
     <>
       <Navbar/>
       <main className='flex-1 overflow-y-auto bg-primary px-12 pt-8'>
-        <SearchInput/>
+        <SearchInput handleSearch={handleSearch}/>
         <Outlet/>
       </main>
     </>
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -3,23 +3,14 @@ import { useNavigate } from "react-router-dom";
 import SearchIcon from '@mui/icons-material/Search';
 import Chevron from '@mui/icons-material/KeyboardArrowDown';
 
-const SearchInput = () => {
+const SearchInput = ({handleSearch}) => {
   const [query, setQuery] = useState("");
   const [criteria, setCriteria] = useState("title");
-  const navigate = useNavigate();
-
   const handleQueryChange = (event) => {
     setQuery(event.target.value);
   };
-
   const handleCriteriaChange = (event) => {
     setCriteria(event.target.value);
-  };
-
-  const handleSearch = () => {
-    if (query) {
-      navigate(`/explore?criteria=${criteria}&query=${query}`);
-    }
   };
 
   return (
@@ -38,12 +29,12 @@ const SearchInput = () => {
         type="text"
         value={query}
         onChange={handleQueryChange}
-        onKeyUp={(e) => e.key === 'Enter' && handleSearch()}
+        onKeyUp={(e) => e.key === 'Enter' && handleSearch(query, criteria)}
         placeholder="Search..."
         className="py-2 px-3 outline-none"
       />
 
-      <button onClick={handleSearch} className="text-main flex-items-center py-2 px-3 bg-white rounded-r-full outline-none">
+      <button onClick={() => handleSearch(query, criteria)} className="text-main flex-items-center py-2 px-3 bg-white rounded-r-full outline-none">
         <SearchIcon sx={{strokeWidth: 0}} fontSize="small"/>
       </button>
     </div>
