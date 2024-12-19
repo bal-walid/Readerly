6283ee803c93c1eb4a8bfad11a7e42fbfc3359ae
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-19 19:35:02 +0100

    Refactor BookStatusDropdown to adapt to filter dropdown in shelf books

--- src/components/BookStatusDropdown.jsx
+++ src/components/BookStatusDropdown.jsx
@@ -1,30 +1,45 @@
-import { useState, useRef } from 'react';
-import TaskAltIcon from '@mui/icons-material/TaskAlt';
-import BookmarksIcon from '@mui/icons-material/Bookmarks';
-import UpdateIcon from '@mui/icons-material/Update';
-import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import { useState, useRef } from "react";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import BookmarksIcon from "@mui/icons-material/Bookmarks";
+import UpdateIcon from "@mui/icons-material/Update";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+import AllInboxIcon from "@mui/icons-material/AllInbox";
 
-const BookStatusDropdown = ({defaultValue, onStatusChange}) => {
+const BookStatusDropdown = ({
+  defaultValue,
+  onStatusChange,
+  filterDropdown = false,
+}) => {
   const statuses = [
     { label: "Completed", icon: <TaskAltIcon />, color: "#388E3C" },
     { label: "Currently Reading", icon: <BookmarksIcon />, color: "#1976D2" },
     { label: "To Be Read", icon: <UpdateIcon />, color: "#B0BEC5" },
     { label: "Did Not Finish", icon: <CancelOutlinedIcon />, color: "#D32F2F" },
   ];
+  const allBooks = {
+    label: "All Books",
+    icon: <AllInboxIcon />,
+    color: "gray",
+  };
 
-  const [selectedOption, setSelectedOption] = useState(statuses.find(status => status.label === defaultValue));
+  const [selectedOption, setSelectedOption] = useState(
+    filterDropdown
+      ? allBooks
+      : statuses.find((status) => status.label === defaultValue)
+  );
 
   const selectRef = useRef(null);
 
   const handleStatusChange = async (event) => {
-    const selectedStatus = statuses.find(status => status.label === event.target.value);
+    const selectedStatus = statuses.find(
+      (status) => status.label === event.target.value
+    );
     try {
       onStatusChange(selectedStatus.label);
       setSelectedOption(selectedStatus);
     } catch (e) {
       console.log(e);
     }
-    
   };
 
   const handleDivClick = () => {
@@ -37,7 +52,10 @@ const BookStatusDropdown = ({defaultValue, onStatusChange}) => {
       onClick={handleDivClick} // Trigger the select focus on div click
     >
       {/* Icon next to the select */}
-      <div className="flex items-center" style={{ color: selectedOption.color }}>
+      <div
+        className="flex items-center"
+        style={{ color: selectedOption.color }}
+      >
         {selectedOption.icon}
       </div>
 
@@ -46,12 +64,17 @@ const BookStatusDropdown = ({defaultValue, onStatusChange}) => {
         ref={selectRef}
         value={selectedOption.label}
         onChange={handleStatusChange}
-        onClick={(e)=> e.stopPropagation()}
+        onClick={(e) => e.stopPropagation()}
         className="focus:outline-none"
         style={{ color: selectedOption.color }}
       >
+        {filterDropdown && <option selected={true}>All Books</option>}
         {statuses.map((status, index) => (
-          <option key={index} value={status.label} style={{ color: status.color }}>
+          <option
+            key={index}
+            value={status.label}
+            style={{ color: status.color }}
+          >
             {status.label}
           </option>
         ))}
