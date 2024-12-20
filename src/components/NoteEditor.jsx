c7400803a8dcb1b014e646c450ede9d2b6d71d7c
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-20 13:23:07 +0100

    Add title input

--- src/components/NoteEditor.jsx
+++ src/components/NoteEditor.jsx
@@ -4,18 +4,33 @@ import "easymde/dist/easymde.min.css";
 import "../assets/styles/easymde-override.css";
 import { useState } from "react";
 
-const options = { 
-  
-}
+const options = {
+  previewClass: ["editor-preview"],
+};
 
 const NoteEditor = () => {
-  const [value, setValue] = useState("# Hello")
+  const [value, setValue] = useState("# Hello");
+  const [title, setTitle] = useState("");
   return (
     <ModalWrapper z={20}>
       <div className="bg-white h-full w-full">
-      <SimpleMDE options={options} value={value} onChange={(value) => setValue(value)}/>
+        <div className="h-[10%]">
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title"
+            className="text-3xl font-header font-semibold px-2 h-full w-full focus:outline-none"
+          />
+        </div>
+        <div className="h-[90%]">
+          <SimpleMDE
+            options={options}
+            value={value}
+            onChange={(value) => setValue(value)}
+          />
+        </div>
       </div>
     </ModalWrapper>
   );
-}
+};
 export default NoteEditor;
