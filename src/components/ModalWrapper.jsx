ecabec5f1da8120e8839905ecdc08da029acfa88
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-20 12:32:54 +0100

    Modify ModalWrapper to accept zIndex prop

--- src/components/ModalWrapper.jsx
+++ src/components/ModalWrapper.jsx
@@ -1,7 +1,8 @@
-const ModalWrapper = ({ onClose, children }) => {
+const ModalWrapper = ({ onClose, children, z = 10}) => {
   return (
     <div
       onClick={onClose}
+      style={{zIndex:z}}
       className="absolute z-10 bg-black bg-opacity-50 top-0 left-0 w-full h-full flex items-center justify-center"
     >
         {children}
