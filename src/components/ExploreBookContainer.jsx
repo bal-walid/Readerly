085dce0e7a40849c85b8d64215a8fe211068b9b0
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-21 18:09:47 +0100

    Implement removing from shelf/wishlist

--- src/components/ExploreBookContainer.jsx
+++ src/components/ExploreBookContainer.jsx
@@ -1,25 +1,51 @@
 import { useBookDetails } from "../hooks/useBooksDetails";
-import { addBookToShelf, addBookToWishlist, removeBookFromShelf, removeBookFromWishlist } from "../utils/db";
+import {
+  addBookToShelf,
+  addBookToWishlist,
+  removeBookFromShelf,
+  removeBookFromWishlist,
+} from "../utils/db";
 import ExploreBookModal from "./ExploreBookModal";
 
 const ExploreBookContainer = ({ book, close }) => {
-  const { bio, bioLoading, synopsis, synopsisLoading, inCollections, inCollectionsLoading } = useBookDetails(book);
+  const {
+    bio,
+    bioLoading,
+    synopsis,
+    synopsisLoading,
+    inCollections,
+    setInCollections,
+    inCollectionsLoading,
+  } = useBookDetails(book);
 
   const handleAddToShelf = (book, bio, synopsis) => {
     addBookToShelf(book, bio, synopsis);
+    setInCollections((inCollections) => {
+      return { ...inCollections, inShelf: true };
+    });
   };
-
+  
   const handleAddToWishlist = (book, bio, synopsis) => {
     addBookToWishlist(book, bio, synopsis);
+    setInCollections((inCollections) => {
+      return { ...inCollections, inWishlist: true };
+    });
   };
-
+  
   const handleRemoveFromShelf = (book) => {
     removeBookFromShelf(book);
-  }
-
+    setInCollections((inCollections) => {
+      return { ...inCollections, inShelf: false };
+    });
+  };
+  
   const handleRemoveFromWishlist = (book) => {
     removeBookFromWishlist(book);
-  }
+    setInCollections((inCollections) => {
+      return { ...inCollections, inWishlist: false };
+    });
+  };
+  
   return (
     <ExploreBookModal
       book={book}
--- src/hooks/useBooksDetails.js
+++ src/hooks/useBooksDetails.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { fetchAuthorBio, fetchSynopsis } from "../utils/api";
 import { bookInCollections } from "../utils/db";
 
 export const useBookDetails = (book) => {
+  const [inCollections, setInCollections] = useState(null);
   const [bio, bioLoading, bioError] = useFetch(fetchAuthorBio, [book.authorId]);
   const [synopsis, synopsisLoading, synopsisError] = useFetch(fetchSynopsis, [book.id]);
-  const [inCollections, inCollectionsLoading, inCollectionsError] = useFetch(bookInCollections, [book.id]);
-  return { bio, bioLoading, bioError, synopsis, synopsisLoading, synopsisError, inCollections, inCollectionsLoading, inCollectionsError };
+  const [inCollectionsDb, inCollectionsLoading, inCollectionsError] = useFetch(bookInCollections, [book.id], (result) => setInCollections(result));
+  return { bio, bioLoading, bioError, synopsis, synopsisLoading, synopsisError, inCollections, setInCollections, inCollectionsLoading, inCollectionsError };
 };
