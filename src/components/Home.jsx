0b63b16e4c196d8ba275871d115e9cba8ce55416
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-12-23 19:37:03 +0100

    Update See More link to point to /recommended

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <h1 className="primary-header capitalize mb-5">{getGreeting() + "!"}</h1>
       <h2 className="secondary-header mb-5 flex max-sm:flex-col items-center">Jump Back In <Link to={'/shelf'} className="ml-auto max-sm:ml-0 text-base hover:text-text-main">Your Library &gt;</Link></h2>
       <CurrentlyReading/>
-      <h2 className="secondary-header my-5 flex max-sm:flex-col items-center">Recommended For You<Link to={'/shelf'} className="ml-auto max-sm:ml-0 text-base hover:text-text-main">See More &gt;</Link></h2>
+      <h2 className="secondary-header my-5 flex max-sm:flex-col items-center">Recommended For You<Link to={'/recommended'} className="ml-auto max-sm:ml-0 text-base hover:text-text-main">See More &gt;</Link></h2>
       <RecommendedForYou/>
       <div className="h-4"></div>
     </>
